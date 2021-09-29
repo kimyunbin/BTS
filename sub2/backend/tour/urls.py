3148def7cb052bf71d4121e5279e3206eafb3f92
Gwanghun-Im <lgh82229047@gmail.com>
2021-09-29 16:28:41 +0900

    [][BE] feat:merge 재 업로드
    Former-commit-id: b47026525afafa48045ba7988578293f902a2137

--- sub2/backend/tour/urls.py
+++ sub2/backend/tour/urls.py
@@ -9,13 +9,7 @@ urlpatterns = [
     path('detail/',views.tour_detail, name = 'tour_detail'),
     path('detail/<int:spot_pk>/',views.tour_review, name = 'tour_review'),
     path('route/',views.route, name = 'tour_route'),
-<<<<<<< HEAD
     path('routerandom/',views.route_random, name = 'tour_route_random'),
     path('city/',views.tour_city, name = 'tour_city')
 
 ]
-=======
-    path('city/',views.tour_city, name = 'tour_city'),
-    path('test/',views.test, )
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> 2c9738092f25e9b01d7ed53f835564972a0cff7f
